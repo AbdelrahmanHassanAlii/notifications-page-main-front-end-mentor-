5f0f49a0083306b310a6c09e74d51f5b2969785a
venom <abdo30201080101694@gmail.com>
2024-01-17 14:20:50 +0200

    handle click on option paragrph and finish all logic

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let unReadedBoxes = document.querySelectorAll(`.box.un-readed`);
 let bullets = document.querySelectorAll(`.box.un-readed .dot`);
 
-
 // handle click on box
 unReadedBoxes.forEach((box) => {
   box.addEventListener("click", () => {
@@ -11,8 +10,27 @@ unReadedBoxes.forEach((box) => {
     bullet.style.display = "none";
     //change the background
     box.style.backgroundColor = "white";
+    // decrease the number of notifications till 0 per condition
+    let currentValue = parseInt(number.innerHTML);
+    if (currentValue > 0) {
+      number.innerHTML = currentValue - 1;
+    }
   });
 });
 
 // handle click on "mark all as read"
+let button = document.querySelector(`.option`);
+let number = document.querySelector(`.number`);
 
+button.addEventListener("click", () => {
+  //remove all bullets
+  bullets.forEach((bullet) => {
+    bullet.style.display = "none";
+  });
+  //change all boxex background colors
+  unReadedBoxes.forEach((box) => {
+    box.style.backgroundColor = "white";
+  });
+  // reset the number of notifications to 0
+  number.innerHTML = "0";
+});
